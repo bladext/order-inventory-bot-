05f38693654f294cb183be5bd9711b602b93e832
bladext <blade_xt@yahoo.com>
2026-01-08 19:29:47 +1300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,13 @@
 import os
+import json
+import logging
+import discord
+from discord.ext import commands
+logging.basicConfig(level=logging.INFO)
+print("Bot file loaded")
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+print("Token loaded:", "YES" if TOKEN else "NO")
+
 
 TOKEN = os.getenv("DISCORD_TOKEN")
